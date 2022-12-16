0d31515d885b2cf607daa8e608e3ab8464b1744c
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-12-16 16:14:28 +0900

    feat(GameController): add 다리 길이 입력 과정

---
+++ src/main/java/bridge/controller/GameController.java
@@ -0,0 +1,26 @@
+package bridge.controller;
+
+import bridge.BridgeRandomNumberGenerator;
+import bridge.model.BridgeGame;
+import bridge.model.BridgeMaker;
+import bridge.module.RepeatModule;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
+public class GameController extends RepeatModule {
+
+    private final BridgeGame bridgeGame;
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+
+    public GameController() {
+        BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        outputView.printStart();
+        int bridgeSize = repeat(inputView::readBridgeSize);
+        bridgeGame = new BridgeGame(bridgeMaker.makeBridge(bridgeSize));
+    }
+
+    public void start() {
+        bridgeGame.print();
+    }
+}
